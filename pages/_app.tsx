3441587c72675d49e980cdf1012644c84b0584cd
franco-pozzi <franco663@hotmail.com>
2022-09-19 10:37:33 -0300

    Add getInitialProps for user theme preference

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,45 @@
-import type { AppProps } from 'next/app'
+import type { AppContext, AppProps } from 'next/app'
 
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import { darkTheme } from '../themes';
 
-function MyApp({ Component, pageProps }: AppProps) {
+import { darkTheme } from '../themes';
+import { lightTheme } from '../themes/light-theme';
+import { customTheme } from '../themes/custom-theme';
+
+interface Props extends AppProps {
+  theme: string;
+}
+
+function MyApp({ Component, pageProps, theme }: Props) {
+
+  console.log(theme)
+
+  const currentTheme = theme === 'light'
+    ? lightTheme
+    : theme === 'dark'
+      ? darkTheme
+      : customTheme
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={currentTheme}>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
 
   )
+}
+
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+
+  const validThemes = ['light', 'dark', 'custom']
+
+  const { theme } = appContext.ctx.req ? (appContext.ctx.req.cookies) : { theme: 'light' }
+
+  return {
+    theme: validThemes.includes(theme) ? theme : 'dark'
+  }
+
 }
 
 export default MyApp
--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,9 +1,54 @@
+
+import { ChangeEvent, FC, useState } from "react"
+
+import axios from 'axios'
+
+import Cookies from 'js-cookie'
+
 import { Layout } from "../components/layouts"
 
-const ThemeChangerPage = () => {
+import { Button, Card, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material"
+
+interface Props {
+  theme: string;
+}
+
+const ThemeChangerPage: FC<Props> = ({ theme }) => {
+  const [currentTheme, setCurrentTheme] = useState('light')
+
+  const onThemeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCurrentTheme(event.target.value)
+
+    Cookies.set('theme', event.target.value)
+  }
+
+  const onClick = async () => {
+    const { data } = await axios.get('/api/hello')
+  }
+
   return (
     <Layout>
-      <h1>Theme Changer</h1>
+      <Card>
+        <CardContent>
+          <FormControl>
+            <FormLabel>
+              Theme
+            </FormLabel>
+            <RadioGroup
+              value={currentTheme}
+              onChange={onThemeChange}
+            >
+              <FormControlLabel value='light' control={<Radio />} label='Light' />
+              <FormControlLabel value='dark' control={<Radio />} label='Dark' />
+              <FormControlLabel value='custom' control={<Radio />} label='Custom' />
+            </RadioGroup>
+          </FormControl>
+
+          <Button onClick={onClick}>
+            Solicitud
+          </Button>
+        </CardContent>
+      </Card>
     </Layout>
   )
 }
